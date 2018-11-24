7f381ff73ecd7a7cc6c44228a5779166c650ff30
Oliver Herst <oliver.herst@googlemail.com>
2018-11-24 18:23:21 +0000

    fix cms

--- blog/ollie.md
+++ blog/ollie.md
@@ -3,4 +3,4 @@ path: /blog/ollie
 date: 2018-11-24T17:22:07.554Z
 title: ollie
 ---
-ollie
+ollie yolo
--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/blog`,
-        name: "markdown-pages",
+        name: 'markdown-pages',
       },
     },
     `gatsby-transformer-remark`,
