bb47ad99f7397c52fec0885beb10b0b02233937c
Oliver Herst <oliver.herst@googlemail.com>
2018-11-24 16:54:18 +0000

    add netlify cms and semantic-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import { Button } from 'semantic-ui-react'
 import Layout from '../components/layout'
 import Image from '../components/image'
+
+const ButtonExampleButton = () => <Button>Click Here</Button>
 
 const IndexPage = () => (
   <Layout>
     <h1>Hello World!</h1>
+    <ButtonExampleButton />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
