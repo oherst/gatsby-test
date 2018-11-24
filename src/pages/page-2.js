eb491119719bfa23383c9987e55f8af4a1b79887
Oliver Herst <oliver.herst@googlemail.com>
2018-11-24 20:26:03 +0000

    add hidden field

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,8 @@ const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <form name="test-form" method="POST" action="#" data-netlify="true">
+    <form name="test-form" method="POST" action="#" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="test-form" />
       <label>
         Name:
       </label>
